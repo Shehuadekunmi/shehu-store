c46bf80f678a82ef7aa88a9e3969a50c5eb60c38
ShehuAdekunmi <sirkumyemo@gmail.com>
2024-09-19 05:46:11 -0700

    uploading setup done

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,7 @@ connectDB();
 
 const app = express()
 
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser());
@@ -45,6 +46,15 @@ app.get("/api/config/paypal", (req, res) => {
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+
+
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
+
 
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
